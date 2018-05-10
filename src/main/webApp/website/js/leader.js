5adaa88cded5c189e3acf13a133a92769ddd12f5
nineSame <746778665@qq.com>
2018-05-10 15:10:46 +0800

    首页下面添加“社会责任”链接 添加leader页面动态获取数据的功能，并用测试数据完成测试。修改leader页面js结构。 解决新闻列表页面刷新“undefined”问题，解决新闻列表搜索选项点击向下箭头无响应问题

--- src/main/webApp/website/js/leader.js
+++ src/main/webApp/website/js/leader.js
@@ -4,8 +4,9 @@
 
 $(function () {
     setFrontLinks();
-    toggleItems();
     setUsersValue();
+    toggleItems();
+
 })
 
 function setFrontLinks() {
@@ -22,7 +23,6 @@ function toggleItems() {
 
     for (var i = 0; i < aSels.length; i++){
         aSels[i].onclick =function () {
-
             $(this).parent().siblings().removeClass('cur');
             $(this).parent().toggleClass('cur');
         }
@@ -39,11 +39,57 @@ function setUsersValue() {
             console.log(1111,json);
             if(json.resCode == 200){
 
+
+                var leaderGroup1 = [];
+                var leaderGroup2 = [];
+                var leaderGroup3 = [];
+
                 var data = json.data;
                 var liHtml = '';
                 for(var i=0;i<data.length;i++){
                     var item = data[i];
+
+                    //数据按照职位阶级分组
+                    if (item.jobtitlenum == 1){
+                        leaderGroup1.push(item);
+                    }else if(item.jobtitlenum == 2){
+                        leaderGroup2.push(item);
+                    }else {
+                        leaderGroup3.push(item);
+                    }
                 }
+
+                //拼接第一组职位html
+                var html1 = '<div class="position">执行董事</div><div class="position_content"><ul>';
+
+                for (var i = 0; i < leaderGroup1.length; i++){
+                        html1 += '<li><div class="top"><div class="top_con clearfix"><div class="leader_name left">'+leaderGroup1[i].name+'</div><div class="leader_position left el">'+leaderGroup1[i].jobtitle+'</div><div class="school left el"></div><div class="ico left"></div></div></div><div class="bottom"><div class="bottom_con"><p><span style="font-family: Arial, Helvetica, sans-serif"><b><span style="font-size: 19px; font-family: '+'宋体'+'">'+leaderGroup1[i].name+'</span></b><span style="font-size:19px;font-family: '+'宋体'+'">,'+leaderGroup1[i].description+'</span></span></p><p><br/></p></div></div></li>'
+                }
+                html1 += '</ul></div>';
+
+                //拼接第二组职位html
+                var html2 = '<div class="position">非执行董事</div><div class="position_content"><ul>';
+
+                for (var i = 0; i < leaderGroup2.length; i++){
+                    html2 += '<li><div class="top"><div class="top_con clearfix"><div class="leader_name left">'+leaderGroup2[i].name+'</div><div class="leader_position left el">'+leaderGroup2[i].jobtitle+'</div><div class="school left el"></div><div class="ico left"></div></div></div><div class="bottom"><div class="bottom_con"><p><span style="font-family: Arial, Helvetica, sans-serif"><b><span style="font-size: 19px; font-family: '+'宋体'+'">'+leaderGroup2[i].name+'</span></b><span style="font-size:19px;font-family: '+'宋体'+'">,'+leaderGroup2[i].description+'</span></span></p><p><br/></p></div></div></li>'
+                }
+
+                html2 += '</ul></div>';
+
+                //拼接第三组职位html
+                var html3 = '<div class="position">执行董事</div><div class="position_content"><ul>';
+
+                for (var i = 0; i < leaderGroup3.length; i++){
+                    html3 += '<li><div class="top"><div class="top_con clearfix"><div class="leader_name left">'+leaderGroup3[i].name+'</div><div class="leader_position left el">'+leaderGroup3[i].jobtitle+'</div><div class="school left el"></div><div class="ico left"></div></div></div><div class="bottom"><div class="bottom_con"><p><span style="font-family: Arial, Helvetica, sans-serif"><b><span style="font-size: 19px; font-family: '+'宋体'+'">'+leaderGroup3[i].name+'</span></b><span style="font-size:19px;font-family: '+'宋体'+'">,'+leaderGroup3[i].description+'</span></span></p><p><br/></p></div></div></li>'
+                }
+                html3 += '</ul></div>';
+
+                //插入html
+                $('#leaders_list').append(html1,html2,html3);
+
+                //设置toggle样式'cur'
+                toggleItems();
+
             }else{
                 alert(json.resMsg);
             }
