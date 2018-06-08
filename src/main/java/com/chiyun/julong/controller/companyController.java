f47726796c9be1c264adae761ddc627442aab9d0
2411929973@qq.com <woaid.t>
2018-06-08 10:53:42 +0800

    公司信息增加字段

--- src/main/java/com/chiyun/julong/controller/companyController.java
+++ src/main/java/com/chiyun/julong/controller/companyController.java
@@ -35,7 +35,7 @@ public class companyController {
 
     @ResponseBody
     @RequestMapping("/company/update")
-    public ApiResult<Object> update(companyEntity companyEntity, MultipartFile gstbfile, HttpSession httpSession) throws Exception {
+    public ApiResult<Object> update(companyEntity companyEntity, MultipartFile gstbfile, MultipartFile gsjstpfile,HttpSession httpSession) throws Exception {
         //判断是否登录
        /* String personid = (String) httpSession.getAttribute("id");
         if (personid.isEmpty()) {
@@ -43,53 +43,71 @@ public class companyController {
         }*/
         //判断是否为管理员
 
-        //判断传进的信息是否为空
-        String name=companyEntity.getGsmz();
-        String address=companyEntity.getGsdz();
-        String num=companyEntity.getGsyb();
-        String phone=companyEntity.getGsdh();
-        String man=companyEntity.getGslxr();
-        if(name==null||name==""||address==null||address==""||num==null||num==""||phone==null||phone==""||man==null||man==""){
-            return ApiResult.FAILURE("传入数据有空数据");
-        }
+//        //判断传进的信息是否为空
+//        String name=companyEntity.getGsmz();
+//        String address=companyEntity.getGsdz();
+//        String num=companyEntity.getGsyb();
+//        String phone=companyEntity.getGsdh();
+//        String man=companyEntity.getGslxr();
+//        System.out.print("-----"+name+"-----"+companyEntity.getJjbt());
+//        if(name==null||name==""||address==null||address==""||num==null||num==""||phone==null||phone==""||man==null||man==""
+//                ||companyEntity.getJjbt()==null||companyEntity.getJjbt()==""
+//                ||companyEntity.getGszz()==null||companyEntity.getGszz()==""
+//                ||companyEntity.getGsjs()==null||companyEntity.getGsjs()==""
+//                ||companyEntity.getRlzpyx()==null||companyEntity.getRlzpyx()==""
+//                ||companyEntity.getRllxr()==null||companyEntity.getRllxr()==""
+//                ||companyEntity.getRllxdh()==null||companyEntity.getRllxdh()==""
+//                ||companyEntity.getXyx()==null||companyEntity.getXyx()==""
+//                ||companyEntity.getYyx()==null||companyEntity.getYyx()==""
+//                ||companyEntity.getZyx()==null||companyEntity.getZyx()==""
+//                ||companyEntity.getZbdh()==null||companyEntity.getZbdh()==""
+//                ||companyEntity.getZbdz()==null||companyEntity.getZbdz()==""
+//                ||companyEntity.getJsdh()==null||companyEntity.getJsdh()==""
+//                ){
+//        return ApiResult.FAILURE("传入数据有空数据");
+//        }
         //通过id查询是否有该信息
         companyEntity companyEntity1 = companyRepository.findById(companyEntity.getId());
         if(companyEntity1==null){
             return ApiResult.FAILURE("未找到该信息");
         }
-
-        if (gstbfile==null||gstbfile.getSize()==0){
-            System.out.print("图标为空");
+        if (gstbfile==null||gstbfile.getSize()==0||gsjstpfile==null||gsjstpfile.getSize()==0){
+            System.out.print("公司图标为空或公司介绍图片为空");
         }else {
         //判断文件上传大小
-        if(gstbfile.getSize()>20971520){
-            return ApiResult.FAILURE("图片超出图标文件大小");
+        if(gstbfile.getSize()>20971520||gsjstpfile.getSize()>20971520){
+            return ApiResult.FAILURE("图片超出文件大小");
         }
         //判断数据库里面是否有图标地址
-        if(companyEntity1.getGstb()!=null&&companyEntity1.getGstb()!=""){
+        if(companyEntity1.getGstb()!=null&&companyEntity1.getGstb()!=""&&companyEntity1.getGsjstp()!=null&&companyEntity1.getGsjstp()!=""){
             //如果有图标，删除原有图标
             int isdel=fileUtil.fileDel(companyEntity1.getGstb(),"company");
-            //判断是否删除成功
-            if(isdel!=1){
-                return ApiResult.FAILURE("图标删除失败");
+            int isdel2=fileUtil.fileDel(companyEntity1.getGsjstp(),"company");
+            //判断图标是否删除成功
+            if(isdel!=1&&isdel2!=1){
+                return ApiResult.FAILURE("公司图标或公司介绍图片删除失败");
             }
             //删除成功后将用户修改的图标上传
             String filename=fileUtil.fileUpload(gstbfile,"company");
+            String filename2=fileUtil.fileUpload(gsjstpfile,"company");
             //判断上传方法返回回来的数据
-            if(filename==null){
-                return ApiResult.FAILURE("图标上传失败");
+            if(filename==null||filename2==null){
+                return ApiResult.FAILURE("公司图标或公司介绍图片上传失败");
             }
             //将文件名保存在数据库
             companyEntity.setGstb(filename);
+            companyEntity.setGsjstp(filename2);
         }else{
             //如果没有，直接上传图标，保存图标名
             String filename=fileUtil.fileUpload(gstbfile,"company");
+            String filename2=fileUtil.fileUpload(gsjstpfile,"company");
             //判断上传方法返回回来的数据
-            if(filename==null){
-                return ApiResult.FAILURE("图标上传失败");
+            if(filename==null||filename2==null){
+                return ApiResult.FAILURE("公司图标或公司介绍图片上传失败");
             }
             //将文件名保存在数据库
             companyEntity.setGstb(filename);
+            companyEntity.setGsjstp(filename2);
         }
         }
         //保存操作
--- src/main/java/com/chiyun/julong/entity/companyEntity.java
+++ src/main/java/com/chiyun/julong/entity/companyEntity.java
@@ -15,6 +15,19 @@ public class companyEntity {
     private String gsyb;//公司邮编
     private String gsdh;//公司电话
     private String gslxr;//公司联系人
+    private String jjbt;//简介标题
+    private String gszz;//公司主旨
+    private String gsjstp;//公司介绍图片
+    private String gsjs;//公司介绍
+    private String rlzpyx;//人力招聘邮箱
+    private String rllxr;//人力联系人
+    private String rllxdh;//人力联系电话
+    private String xyx;//x邮箱
+    private String yyx;//y邮箱
+    private String zyx;//z邮箱
+    private String zbdh;//招标电话
+    private String zbdz;//招标地址
+    private String jsdh;//技术电话
 
     @Id
     @Column(name = "ID")
@@ -88,6 +101,135 @@ public class companyEntity {
         this.gslxr = gslxr;
     }
 
+    @Basic
+    @Column(name = "introtitle")
+    public String  getJjbt() {
+        return jjbt;
+    }
+
+    public void setJjbt(String jjbt) {
+        this.jjbt = jjbt;
+    }
+
+    @Basic
+    @Column(name = "purpose")
+    public String getGszz() {
+        return gszz;
+    }
+
+    public void setGszz(String gszz) {
+        this.gszz = gszz;
+    }
+
+    @Basic
+    @Column(name = "photo")
+    public String getGsjstp() {
+        return gsjstp;
+    }
+
+    public void setGsjstp(String gsjstp) {
+        this.gsjstp = gsjstp;
+    }
+
+    @Basic
+    @Column(name = "introduction")
+    public String getGsjs() {
+        return gsjs;
+    }
+
+    public void setGsjs(String gsjs) {
+        this.gsjs = gsjs;
+    }
+
+    @Basic
+    @Column(name = "hremail")
+    public String getRlzpyx() {
+        return rlzpyx;
+    }
+
+    public void setRlzpyx(String rlzpyx) {
+        this.rlzpyx = rlzpyx;
+    }
+
+    @Basic
+    @Column(name = "hrname")
+    public String getRllxr() {
+        return rllxr;
+    }
+
+    public void setRllxr(String rllxr) {
+        this.rllxr = rllxr;
+    }
+
+    @Basic
+    @Column(name = "hrphone")
+    public String getRllxdh() {
+        return rllxdh;
+    }
+
+    public void setRllxdh(String rllxdh) {
+        this.rllxdh = rllxdh;
+    }
+
+    @Basic
+    @Column(name = "xemail")
+    public String getXyx() {
+        return xyx;
+    }
+
+    public void setXyx(String xyx) {
+        this.xyx = xyx;
+    }
+
+    @Basic
+    @Column(name = "yemail")
+    public String getYyx() {
+        return yyx;
+    }
+
+    public void setYyx(String yyx) {
+        this.yyx = yyx;
+    }
+
+    @Basic
+    @Column(name = "zemail")
+    public String getZyx() {
+        return zyx;
+    }
+
+    public void setZyx(String zyx) {
+        this.zyx = zyx;
+    }
+
+    @Basic
+    @Column(name = "tenderphone")
+    public String getZbdh() {
+        return zbdh;
+    }
+
+    public void setZbdh(String zbdh) {
+        this.zbdh = zbdh;
+    }
+
+    @Basic
+    @Column(name = "tenderaddress")
+    public String getZbdz() {
+        return zbdz;
+    }
+
+    public void setZbdz(String zbdz) {
+        this.zbdz = zbdz;
+    }
+
+    @Basic
+    @Column(name = "technologycall")
+    public String getJsdh() {
+        return jsdh;
+    }
+
+    public void setJsdh(String jsdh) {
+        this.jsdh = jsdh;
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -100,13 +242,26 @@ public class companyEntity {
                 Objects.equals(gsdz, that.gsdz) &&
                 Objects.equals(gsyb, that.gsyb) &&
                 Objects.equals(gsdh, that.gsdh) &&
-                Objects.equals(gslxr, that.gslxr);
+                Objects.equals(gslxr, that.gslxr)&&
+                Objects.equals(jjbt, that.jjbt)&&
+                Objects.equals(gszz, that.gszz) &&
+                Objects.equals(gsjstp, that.gsjstp) &&
+                Objects.equals(gsjs, that.gsjs) &&
+                Objects.equals(rlzpyx, that.rlzpyx)&&
+                Objects.equals(rllxr, that.rllxr)&&
+                Objects.equals(rllxdh, that.rllxdh) &&
+                Objects.equals(xyx, that.xyx) &&
+                Objects.equals(yyx, that.yyx) &&
+                Objects.equals(zyx, that.zyx)&&
+                Objects.equals(zbdh, that.zbdh) &&
+                Objects.equals(zbdz, that.zbdz) &&
+                Objects.equals(jsdh, that.jsdh);
     }
 
     @Override
     public int hashCode() {
 
-        return Objects.hash(id, gsmz, gstb, gsdz, gsyb, gsdh, gslxr);
+        return Objects.hash(id, gsmz, gstb, gsdz, gsyb, gsdh, gslxr, jjbt, gszz, gsjstp, gsjs, rlzpyx, rllxr, rllxdh, xyx, yyx, zyx, zbdh, zbdz, jsdh);
     }
 
 }
